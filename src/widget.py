def8f42b088b4cf3b8ffcc09b169c1e187166686
andrey yurov <yurov.ai@yandex.ru>
2024-10-29 11:42:59 +0300

    закончена работа с кодом и проверкой линтерами

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,6 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
-
-def mask_account_card(incoming_data):
+def mask_account_card(incoming_data: str) -> str:
     numbers = ""
     text = ""
     final_masks_number = ""
