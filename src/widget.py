019f1f3c3a0eda09941fcc19dc1f81b98a1dd07c
andrey yurov <yurov.ai@yandex.ru>
2024-10-29 11:13:25 +0300

    добавил функцию в widget.py перед проверкой линтерами

---
+++ src/widget.py
@@ -0,0 +1,20 @@
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+
+def mask_account_card(incoming_data):
+    numbers = ""
+    text = ""
+    final_masks_number = ""
+    for symbol in incoming_data:
+        if symbol.isdigit():
+            numbers += symbol
+        elif symbol.isalpha() or symbol == " ":
+            text += symbol
+
+    if len(numbers) == 16:
+        final_masks_number += get_mask_card_number(numbers)
+    else:
+        final_masks_number += get_mask_account(numbers)
+
+    return f"{text}{final_masks_number}"
