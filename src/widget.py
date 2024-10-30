a0d70a4371c905c4ed9c523d2c47e4bd2b2cde16
andrey yurov <yurov.ai@yandex.ru>
2024-10-30 13:12:58 +0300

    дописал вторую функцию get_date

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+
 def mask_account_card(incoming_data: str) -> str:
     numbers = ""
     text = ""
@@ -16,3 +17,20 @@ def mask_account_card(incoming_data: str) -> str:
         final_masks_number += get_mask_account(numbers)
 
     return f"{text}{final_masks_number}"
+
+
+def get_date(incoming_date: str) -> str:
+    date_string = ""
+    for i in incoming_date:
+        if i.isdigit() and len(date_string) < 10:
+            date_string += i
+        elif i == "-" and len(date_string) < 10 or i == "." and len(date_string) < 10:
+            date_string += "."
+
+    final_date_string = date_string[8:11] + date_string[4:8] + date_string[0:4]
+
+    return final_date_string
+
+
+fact_date = "2024-03-11T02:26:18.671407"
+print(get_date(fact_date))
