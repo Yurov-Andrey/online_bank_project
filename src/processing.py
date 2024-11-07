936506b9507a9bfd0a32e7acd3425e1298d45ebb
Yurov-Andrey <thefesman@gmail.com>
2024-11-07 17:47:01 +0300

    Написана функция filter_by_state и проверена ее работоспособность

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,20 @@
-def filter_by_state():
-    pass
+def filter_by_state(incoming_list: list[dict]) -> list[dict]:
+    filtered_list = []
+    for any_dict in incoming_list:
+        for key, value in any_dict.items():
+            if value == 'EXECUTED':
+                filtered_list.append(any_dict)
+
+    return filtered_list
 
 
 def sort_by_date():
     pass
+
+
+random_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+print(filter_by_state(random_dict))
