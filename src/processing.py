c627dd2c87a449bdef1db54b9ad88253b02aac28
Yurov-Andrey <thefesman@gmail.com>
2024-11-09 10:22:40 +0300

    Добавлены описания (docstring) для функций filter_by_state и sort_by_date. Осуществлена проверка линтерами (black, mypy, isort, flake8).

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,28 @@
-def filter_by_state(incoming_list: list[dict]) -> list[dict]:
+def filter_by_state(incoming_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению"""
     filtered_list = []
     for any_dict in incoming_list:
         for key, value in any_dict.items():
-            if value == 'EXECUTED':
+            if value == state:
                 filtered_list.append(any_dict)
 
     return filtered_list
 
 
-def sort_by_date(incoming_list: list[dict], sorting_order = True) -> list[dict]:
-    sort_dictionary = sorted(incoming_list, key=lambda x: x['date'], reverse = sorting_order)
+def sort_by_date(incoming_list: list[dict], sorting_order: bool = True) -> list[dict]:
+    """Функция должна возвращать новый список, отсортированный по дате (date)"""
+    sort_dictionary = sorted(incoming_list, key=lambda x: x["date"], reverse=sorting_order)
 
     return sort_dictionary
 
 
-random_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+random_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 print(filter_by_state(random_dict))
 print(sort_by_date(random_dict))
