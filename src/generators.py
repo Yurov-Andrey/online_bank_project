9fe2d3503bab32975501b3ba047283bfc1ffa394
Yurov Andrey <thefesman@gmail.com>
2024-11-22 15:23:52 +0300

    Написана первая рабочая версия функции filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,15 @@
-def filter_by_currency():
-    pass
+from typing import Iterator
+
+
+def filter_by_currency(incoming_list: list[dict[str, int, dict[str, int, dict, [str, int, dict, [str, int]]]]], currency_type: str = "USD") -> Iterator[
+    dict[str, dict[str, int], int]]:
+    """Функция, которая принимает на вход список словарей, представляющих транзакции.
+Функция должна возвращать итератор, который поочередно выдает транзакции,
+где валюта операции соответствует заданной (например, USD)."""
+
+    for transaction in incoming_list:
+        if transaction["operationAmount"]["currency"]["name"] == currency_type:
+            yield transaction
 
 
 def transaction_descriptions():
