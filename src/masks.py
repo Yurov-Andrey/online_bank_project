0c0d7a37f82d46344d583fe9f7270d230da7cc9f
Yurov Andrey <thefesman@gmail.com>
2024-12-08 15:41:07 +0300

    Написаны логеры для функций модулей masks.py и utils.py.о

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,12 @@
 import logging
+import os
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/masks.log")
+file_handler = logging.FileHandler(f"{log_dir}/masks.log", mode="w")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(lineno)d: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -11,38 +15,36 @@ logger.addHandler(file_handler)
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номера принимаемых карт"""
 
-    try:
-        logger.info(f"Старт работы функции {get_mask_card_number}")
-        if len(card_number) != 16:
-            logger.debug(f"Проверяем длину строки {card_number}.")
-            raise ValueError("Не верная длина номера карты")
+    logger.info(f"Старт работы функции {get_mask_card_number}")
+    if len(card_number) != 16:
+        logger.debug(f"Проверяем длину строки {card_number}.")
+        logger.error("Не верная длина номера карты")
+        raise ValueError("Не верная длина номера карты")
 
-        if not card_number.isdigit():
-            logger.debug(f"Проверяем состоит ли строка {card_number} только из цифр.")
-            raise ValueError("Номер карты должен состоять только из цифр")
+    if not card_number.isdigit():
+        logger.debug(f"Проверяем состоит ли строка {card_number} только из цифр.")
+        logger.error("Номер карты должен состоять только из цифр")
+        raise ValueError("Номер карты должен состоять только из цифр")
 
-        logger.info(f"Функция {get_mask_account} завершила работу и вернула результат.")
-        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
-
-    except ValueError as e:
-        logger.error(f"Возникла ошибка: {e}")
+    masked_card_number = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
+    logger.info(f"Функция завершила работу и вернула результат: {masked_card_number}")
+    return masked_card_number
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскирует номера принимаемых счетов"""
 
-    try:
-        logger.info(f"Старт работы функции {get_mask_account}")
-        if len(account_number) != 20:
-            logger.debug(f"Проверяем длину строки {account_number}.")
-            raise ValueError("Не верная длина номера счета")
+    logger.info(f"Старт работы функции {get_mask_account}")
+    if len(account_number) != 20:
+        logger.debug(f"Проверяем длину строки {account_number}.")
+        logger.error("Не верная длина номера счета")
+        raise ValueError("Не верная длина номера счета")
 
-        if not account_number.isdigit():
-            logger.debug(f"Проверяем состоит ли строка {account_number} только из цифр.")
-            raise ValueError("Номер счета должен состоять только из цифр")
+    if not account_number.isdigit():
+        logger.debug(f"Проверяем состоит ли строка {account_number} только из цифр.")
+        logger.error("Номер счета должен состоять только из цифр")
+        raise ValueError("Номер счета должен состоять только из цифр")
 
-        logger.info(f"Функция {get_mask_account} завершила работу и вернула результат.")
-        return f"**{account_number[-5:]}"
-
-    except ValueError as e:
-        logger.error(f"Возникла ошибка: {e}")
+    masked_account_number = f"**{account_number[-5:]}"
+    logger.info(f"Функция завершила работу и вернула результат: {masked_account_number}")
+    return masked_account_number
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,14 @@
 import json
 import logging
+import os
 from typing import Any
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/masks.log")
+file_handler = logging.FileHandler(f"{log_dir}/utils.log", mode="w")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(lineno)d: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -16,29 +20,35 @@ def processing_json_dict(file_path: Any) -> list | str:
     функция возвращает пустой список."""
 
     try:
-        logger.info(f"Старт работы функции {processing_json_dict}")
+        logger.info(f"Старт работы функции {processing_json_dict}, принят файл по пути {file_path}")
         with open(file_path, "r", encoding="utf-8") as file:
             contents = json.load(file)
-            logger.info(f"Открыт и прочитан файл {file_path}")
+            logger.info(f"Открыт и прочитан файл {file}")
 
-            logger.debug(f'Проверка типа данных содержимого файла {file_path}')
+            logger.debug(f"Проверка типа данных содержимого файла.")
             if not isinstance(contents, list):
                 empty_list: list = []
-                logger.warning(f'Проверка не пройдена, создан пустой список {empty_list}')
+                logger.warning(
+                    f"Проверка не пройдена, содержимое не является списком. Создан пустой список {empty_list}"
+                )
                 return empty_list
 
-
             elif len(contents) == 0:
-                logger.debug(f'Проверка типа данных содержимого файла {file_path}')
                 empty_list = []
+                logger.warning(
+                    f"Проверка не пройдена, содержимое является пустым списком. Создан пустой список {empty_list}"
+                )
                 return empty_list
 
             else:
+                logger.info(f"Функция {processing_json_dict} завершила работу и вернула результат.")
                 return contents
 
     except FileNotFoundError:
+        logger.error(f"Запрашиваемый файл не найден или указан некорректный путь ({file_path}) к файлу")
         return f"Запрашиваемый файл не найден или указан некорректный путь ({file_path}) к файлу"
 
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
         empty_list = []
+        logger.error(f"Ошибка {e}. Создан пустой список {empty_list}")
         return empty_list
