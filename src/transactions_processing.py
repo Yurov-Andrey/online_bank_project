8738c550df586b442e4f6b578f36a11bd37c847f
Yurov Andrey <thefesman@gmail.com>
2024-12-12 16:39:38 +0300

    Установлены новые зависимости, создан модуль transactions_processing.py и написана функция def processing_csv()

---
+++ src/transactions_processing.py
@@ -0,0 +1,68 @@
+import csv
+import logging
+import os
+import pandas as pd
+from typing import Any
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+logger = logging.getLogger("transaction_processing_logger")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(f"{log_dir}/transaction_processing.log", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(lineno)d: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def processing_csv(file_path: Any) -> list[dict] | str:
+    """ Функция для считывания финансовых операций из CSV принимает путь к файлу CSV в качестве аргумента и
+     считывает финансовые операции из CSV, после чего выдает список словарей с транзакциями. """
+    logger.info(f"Старт работы функции {processing_csv}, принят файл по пути {file_path}")
+    try:
+
+        if os.path.splitext(file_path)[1].lower() == '.csv':
+            with open(file_path) as file:
+                logger.info(f"Открыт и прочитан файл {file}")
+                reader = csv.DictReader(file, delimiter=';')
+                result = list(reader)
+
+            logger.debug("Проверка типа данных содержимого файла.")
+            for x in result:
+                if not isinstance(x, dict):
+                    empty_list: list = []
+                    logger.warning(
+                        f"Проверка не пройдена, содержимое не является списком. Создан пустой список {empty_list}"
+                    )
+                    return empty_list
+
+                elif len(result) == 0:
+                    empty_list = []
+                    logger.warning(
+                        f"Проверка не пройдена, содержимое является пустым списком. Создан пустой список {empty_list}"
+                    )
+                    return empty_list
+
+    except FileNotFoundError:
+        logger.error(f"Запрашиваемый файл не найден или указан некорректный путь ({file_path}) к файлу")
+        return f"Запрашиваемый файл не найден или указан некорректный путь ({file_path}) к файлу"
+
+    except ValueError:
+        logger.error("Файл по указанному пути не является csv файлом")
+        return "Файл по указанному пути не является csv файлом"
+
+    logger.info(f"Функция {processing_csv} завершила работу и вернула результат.")
+    return result
+
+
+
+
+
+
+
+
+
+
+
+my_path_csv = '../data/transactions.csv'
+print(processing_csv(my_path_csv))
